8434fd80e8ede8e25e463e51c573a489452b9b5b
Master-COLLiDER <probaldeepsaikia@hotmail.com>
2021-12-01 11:00:55 +0530

    clearing

--- Collection.h
+++ Collection.h
@@ -1,49 +1,8 @@
-#ifndef COLLECTION_H
-#define COLLECTION_H
+#ifndef DYNAMIC_COLLECTION_H
+#define DYNAMIC_COLLECTION_H
 
 
-#define CURRENT_TYPE float
+class Collection {};
 
 
-class Collection {
-    int MAX, count;
-    CURRENT_TYPE *data;
-public:
-    Collection();
-
-    Collection(int n);
-
-    ~Collection();
-
-    int getMax() const;
-
-    bool isEmpty();
-
-    int insert_pos(CURRENT_TYPE what, int position);
-
-    int insert_beginning(CURRENT_TYPE what);
-
-    int insert_end(CURRENT_TYPE what);
-
-    int insert_after(CURRENT_TYPE what, CURRENT_TYPE after_which);
-
-    int insert_before(CURRENT_TYPE what, CURRENT_TYPE before_which);
-
-    CURRENT_TYPE delete_beginning();
-
-    CURRENT_TYPE delete_end();
-
-    CURRENT_TYPE delete_pos(int position);
-
-    CURRENT_TYPE delete_data(CURRENT_TYPE which);
-
-    int indexOf(CURRENT_TYPE which);
-
-    void bubbleSort();
-
-    void displayData();
-
-};
-
-
-#endif //COLLECTION_H
+#endif //DYNAMIC_COLLECTION_H
--- main.cpp
+++ main.cpp
@@ -1,66 +1,8 @@
 #include <iostream>
 #include "Collection.h"
 
-void printMenu();
-
-
 int main() {
-
-    int n, choice = 0, pos;
-    CURRENT_TYPE data;
-
-    std::cout << "Enter the Collection size: ";
-    std::cin >> n;
-
-    Collection collection = Collection(n);
-
-
-    while (choice != 5) {
-        printMenu();
-        std::cin >> choice;
-
-        switch (choice) {
-            case 1:
-                std::cout << "Enter the value to insert: ";
-                std::cin >> data;
-                std::cout << "In which position: ";
-                std::cin >> pos;
-                if (collection.insert_pos(data, pos)) {
-                    std::cout << "Successfully Inserted " << data << " in " << pos << "-th position.";
-                } else {
-                    std::cout << "Failed to insert " << data << " in " << pos << "-th position";
-                }
-                break;
-            case 2:
-                collection.displayData();
-                break;
-            case 3:
-                collection.bubbleSort();
-                break;
-            case 4:
-                std::cout << "Enter the position for the value to delete: ";
-                std::cin >> pos;
-                std::cout << "Deleted " << collection.delete_pos(pos) << " in " << pos << "-th position.";
-
-                break;
-            case 5:
-                break;
-            default:
-                std::cout << "\nInvalid Choice!!!";
-        }
-    }
-
+    std::cout<<"Hello World!"<<std::endl;
     return 0;
 
-}
-
-void printMenu() {
-    std::cout << "\nRoll No.: CSM21002\n";
-    std::cout << ":::Collection Class Menu:::\n";
-    std::cout << "1. Insert\n";
-    std::cout << "2. Display\n";
-    std::cout << "3. Bubble Sort\n";
-    std::cout << "4. Delete\n";
-    std::cout << "5. Exit\n";
-    std::cout << "Enter your Choice: ";
 }
