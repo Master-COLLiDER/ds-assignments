79185ab40b87812b98bab3fba932d5c8df637c14
Master-COLLiDER <probaldeepsaikia@hotmail.com>
2021-12-02 22:29:09 +0530

    geagae

---
+++ OrderedPair.h
@@ -0,0 +1,32 @@
+#ifndef ORDERED_PAIR_H
+#define ORDERED_PAIR_H
+
+
+class OrderedPair {
+    int x, y;
+public:
+    OrderedPair();
+
+    OrderedPair(int x, int y);
+
+    OrderedPair(const OrderedPair &orderedPair);
+
+    ~OrderedPair();
+
+    void setX(int x);
+
+    void setY(int y);
+
+    void set(int x = 0, int y = 0);
+
+    int getX();
+
+    int getY();
+
+    void show();
+
+
+};
+
+
+#endif //ORDERED_PAIR_H
